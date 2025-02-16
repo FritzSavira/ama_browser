1b2b8f4c17b6f68286c1a90b296936a8ae458cbe
FritzSavira <Friedemann.Decker@online.de>
2025-02-16 21:06:33 +0100

    index.html: Agenten in Kleinschreibung anzeigen style.css: feedback-negative i:hover hinzu

--- static/js/main.js
+++ static/js/main.js
@@ -143,8 +143,8 @@ document.addEventListener('DOMContentLoaded', () => {
                             });
                             if (feedbackResponse.ok) {
                                 alert('Danke für dein Feedback!');
-                                feedbackOptionsContainer.remove() // Neu hinzu
-                                // feedbackPrompt.remove();    // Remove the feedback prompt
+                                feedbackPrompt.remove();    // Remove the feedback prompt
+                                feedbackOptionsContainer.remove() // Remove the feedback options prompt
                                 freitextContainer.remove(); // Remove the freitext form
                             } else {
                                 alert('Fehler beim Senden des Feedbacks.');
